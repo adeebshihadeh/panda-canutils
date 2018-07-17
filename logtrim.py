448688d7dadbe4d8d39886fc214d784aba628b53
quillford <quillford@users.noreply.github.com>
2018-07-16 20:49:03 -0700

    add logtrim

---
+++ logtrim.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python2
+
+import sys
+
+if len(sys.argv) != 4:
+  print "usage: ./logprint.py log.csv <start time> <end time>"
+  sys.exit()
+
+start = float(sys.argv[2])
+stop = float(sys.argv[3])
+log = open(sys.argv[1]).readlines()
+logg = log
+
+with open(sys.argv[1].split(".csv")[0] + "_trimmed.csv", "w") as f:
+  f.write(log[0])
+  for i in range(1, len(log)):
+    try:
+      t = float(log[i].split(",")[0])
+    except:
+      continue
+    if t >= start and t <= stop:
+      print t
+      f.write(log[i])
