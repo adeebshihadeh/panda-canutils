bdb21d3fa292955daa0c2d1879892b06380b2096
quillford <quillford@users.noreply.github.com>
2018-12-29 23:38:39 -0800

    upadte logtrim

--- logtrim.py
+++ logtrim.py
@@ -1,24 +1,24 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python2.7
 
 import sys
 
-if len(sys.argv) != 4:
-  print "usage: ./logprint.py log.csv <start time> <end time>"
-  sys.exit()
+if __name__ == "__main__":
+  if len(sys.argv) != 4:
+    print "usage: {} log.csv <start time> <end time>".format(sys.argv[0])
+    exit(-1)
 
-start = float(sys.argv[2])
-stop = float(sys.argv[3])
-log = open(sys.argv[1]).readlines()
-logg = log
+  start = float(sys.argv[2])
+  stop = float(sys.argv[3])
+  log = open(sys.argv[1]).readlines()
 
-with open(sys.argv[1].split(".csv")[0] + "_trimmed.csv", "w") as f:
-  f.write(log[0])
-  for i in range(1, len(log)):
-    try:
-      t = float(log[i].split(",")[0])
-    except:
-      continue
-    if t >= start and t <= stop:
-      f.write(log[i])
-    elif t > stop:
-      break
+  with open(sys.argv[1].split(".csv")[0] + "_trimmed.csv", "w") as f:
+    f.write(log[0])
+    for line in log[1:]:
+      if len(line.split(",")) != 4:
+        continue
+      t = float(line.split(",")[0])
+      if t >= start and t <= stop:
+        f.write(line)
+      elif t > stop:
+        break
+
