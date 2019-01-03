13ef485f9361e2e9283bfe68990f80f8d8e9a32d
quillford <quillford@users.noreply.github.com>
2019-01-03 14:28:12 -0800

    clean up cancompare

--- cancompare.py
+++ cancompare.py
@@ -1,99 +1,57 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python2.7
 
 import sys
-from binascii import hexlify
 from itertools import izip_longest
-from multiprocessing import Pool
+from cabana import Log
 
-def bytes_to_bits(b):
-  ret = []
+def safe_hex(a):
+  return a if not isinstance(a, (int, long)) else hex(a)
 
-  for i in xrange(0, len(b), 2):
-    ret += list(bin(int(b[i:i+2], base=16))[2:].zfill(8))
+# compare the addrs between two logs
+def addr_diff(log1, log2):
+  addrs1 = sorted(log1.get_addrs().keys())
+  addrs2 = sorted(log2.get_addrs().keys())
 
-  return ret
-
-def id_diff(log1, log2):
-  log1 = log1[1:]
-  log1 = [l.split(",")[1] for l in log1 if len(l.split(",")) == 4]
-  log1 = list(set(log1))
-  log2 = log2[1:]
-  log2 = [l.split(",")[1] for l in log2 if len(l.split(",")) == 4]
-  log2 = list(set(log2))
-
-
-  shared = list()
-  shared = [i for i in log1 if i in log2]
-  log1 = [i for i in log1 if i not in shared]
-  log2 = [i for i in log2 if i not in shared]
+  shared = [int(i) for i in addrs1 if i in addrs2]
+  addrs1 = [int(i) for i in addrs1 if i not in shared]
+  addrs2 = [int(i) for i in addrs2 if i not in shared]
 
   print len(shared), "shared"
-  print len(log1), "only in log1"
-  print len(log2), "only in log2"
+  print len(addrs1), "only in log1"
+  print len(addrs2), "only in log2"
 
-  print "-"*20
-  print "shared\tlog1\tlog2"
-  print "-"*20
+  print "-"*20, "\nshared\tlog1\tlog2\n", "-"*20
+  for s, l1, l2 in izip_longest(shared, addrs1, addrs2, fillvalue=""):
+    print safe_hex(s), "\t", safe_hex(l1), "\t", safe_hex(l2)
 
-  for s, l1, l2 in izip_longest(shared, log1, log2):
-    print "{}\t{}\t{}".format("" if not s else hex(int(s)), "" if l1 is None else hex(int(l1)), "" if not l2 else hex(int(l2)))
+# only outputs diff on addresses that appear in both logs
+def bit_diff(log1, log2):
+  diff1 = log1.get_bit_diff()
+  diff2 = log2.get_bit_diff()
 
-def get_bits(log):
-  messages = {}
-  for line in log[1:]:
-    line = line.strip().split(",")[1:]
-
-    if len(line) == 3:
-      addr, _, b = line
-
-      if addr not in messages:
-        messages[addr] = bytes_to_bits(b)
-      else:
-        bits = bytes_to_bits(b)
-        for i in range(len(bits)):
-          if messages[addr][i] != 2:
-            messages[addr][i] = 2 if messages[addr][i] != bits[i] else bits[i]
-  return dict(sorted(messages.items()))
-
-def bit_diff(log1, log2, save):
-  pool = Pool()
-
-  seen = pool.map(get_bits, [log1, log2])
-
-  for addr in seen[0]:
-    if addr not in seen[1]:
+  for addr, bits in sorted(diff1.iteritems()):
+    if addr not in diff2:
       continue
 
-    bits = []
-    for i in range(len(seen[0][addr])):
-      if seen[0][addr][i] != seen[1][addr][i]:
-        bits.append(str(i + 1))
-    if len(bits):
-      print "diff on addr", hex(int(addr)), "\n   bits", ', '.join(bits)
-
-  if save:
-    with open("output", "w") as f:
-      for log in seen:
-        f.write("{0} log {0}\n".format("="*10))
-        for addr in log:
-          f.write("\t{}\n".format(hex(int(addr))))
-          for i in range(len(log[addr])/8):
-            f.write("\t\t{}\n".format(" ".join(map(str, log[addr][i*8:i*8+8]))))
-
-
+    diff = []
+    for i, b in enumerate(bits):
+      if b != diff2[addr][i]: diff.append(str(i+1))
+    if len(diff):
+      print "diff on addr", hex(addr), "\n\tbits", ", ".join(diff)
 
 if __name__ == "__main__":
   if len(sys.argv) < 2:
     print "usage: python cancompare.py <log 1> <log 2>"
     print "\nlogs are exported from cabana"
-    print "add --bits arg at the end to run a diff on the bits instead of the ids"
-    print "add --save arg at the end to save the bit data for every message to an output file"
+    print "add --bits arg at the end to run a diff on the bits instead of the addrs"
     print "**warning** cancompare ignores bus number"
-    sys.exit()
+    exit(-1)
 
-  csv1 = open(sys.argv[1]).readlines()
-  csv2 = open(sys.argv[2]).readlines()
+  log1 = Log(open(sys.argv[1]).read())
+  log2 = Log(open(sys.argv[2]).read())
+
   if "--bits" in sys.argv:
-    bit_diff(csv1, csv2, "--save" in sys.argv)
+    bit_diff(log1, log2)
   else:
-    id_diff(csv1, csv2)
+    addr_diff(log1, log2)
+
